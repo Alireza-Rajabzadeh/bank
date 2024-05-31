3995766c14bd74fd6867eb72fcc5affddca3e2cf
Alireza <orginalireza@gmail.com>
2024-05-31 16:03:00 +0330

    add card api added

--- app/Models/Accounts.php
+++ app/Models/Accounts.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Accounts extends Model
 {
     use HasFactory;
+    protected $guarded = []; 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardsController;
+
+Route::group(['prefix' => '/client/', 'middleware' => 'auth'], function () {
+
+    Route::group(['prefix' => '/cards/'], function () {
+
+        Route::post('/', [CardsController::class, 'addCard'])->name('add-card');
+    });
+});
